87d975557b58bf5a85e0704e7e25894a6fb54dbc
chrishansen69 <cadrekucra@gmx.com>
2016-11-06 13:42:56 -0200

    init (reverted from commit 27501bff141305d883bf6288abd269376a74bed3)

--- publish.cmd
+++
@@ -1,4 +0,0 @@
-git subtree pull --prefix docs origin master && git subtree push --prefix docs origin master
-if errorlevel 1 (
-   exit /b %errorlevel%
-)
--- source/index.md
+++
@@ -1,9 +0,0 @@
----
-title: index
-date: 2016-11-05 23:48:08
-noSidebar: true
----
-
-# home page -- add something here
-
-abcd
